abce241eb8fc79cabe0b099df7ae5ee09aa9fa06
Dandun Adi <dandun.adi@gmail.com>
2021-04-26 16:00:29 +0700

    add mobile navigation

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
-const menus = document.querySelectorAll(".navbar__dropdown");
-const buttons = document.querySelectorAll(".navbar__dropdown button");
+const desktopMenus = document.querySelectorAll(".navbar__dropdown");
+const desktopButtons = document.querySelectorAll(".navbar__dropdown button");
+const mobileMenus = document.querySelectorAll(".mobile__nav__dropdown");
+const mobileButtons = document.querySelectorAll(
+  ".mobile__nav__dropdown button"
+);
 
-for (var i in buttons) {
-  buttons[i].onclick = function () {
-    const menuClasses = this.parentElement.classList;
-    if (menuClasses.contains("open")) {
-      menuClasses.remove("open");
-    } else {
-      menus.forEach((menu) => {
-        menu.classList.remove("open");
-      });
-      menuClasses.add("open");
-    }
-  };
-}
+const hamburgerBtn = document.querySelector(".hamburger");
+const mobileNav = document.querySelector(".mobile");
+
+const addMenuHandler = (buttons, menus) => {
+  for (var i in buttons) {
+    buttons[i].onclick = function () {
+      const menuClasses = this.parentElement.classList;
+      if (menuClasses.contains("open")) {
+        menuClasses.remove("open");
+      } else {
+        menus.forEach((menu) => {
+          menu.classList.remove("open");
+        });
+        menuClasses.add("open");
+      }
+    };
+  }
+};
+
+hamburgerBtn.onclick = function () {
+  hamburgerBtn.classList.toggle("close");
+  mobileNav.classList.toggle("hide");
+};
+
+addMenuHandler(desktopButtons, desktopMenus);
+addMenuHandler(mobileButtons, mobileMenus);
